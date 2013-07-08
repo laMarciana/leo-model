a1ed829ff4169b7d7f3ab06190b37231bc4a1bc5
marc <marc@lamarciana.com>
2013-07-08 18:20:10 +0200

    require library from main lib file

--- lib/puzzblog-model.rb
+++ lib/puzzblog-model.rb
@@ -1,3 +1,12 @@
+%w[
+dm-core
+dm-rspec
+dm-timestamps
+dm-validations
+].each do |lib|
+  require lib
+end
+
 require_relative 'puzzblog/model/post'
 require_relative 'puzzblog/model/category'
 require_relative 'puzzblog/model/author'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,3 @@
-%w[
-dm-core
-dm-rspec
-dm-timestamps
-dm-validations
-].each do |lib|
-  require lib
-end
-
 require_relative '../lib/puzzblog-model'
 
 RSpec.configure do |config|
